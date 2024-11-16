ddfc0f4e7ba5458edbf87e1785b98c02fba7793d
isaiah.v <isaiah.v@comcast.net>
2024-11-16 11:44:23 -0800

    set default images

--- terraform/var.tf
+++ terraform/var.tf
@@ -39,18 +39,15 @@ variable "postgres_root_user" {
   description = "The PostgreSQL root username"
   type        = string
 }
-
 variable "postgres_root_password" {
   description = "The PostgreSQL root password"
   type        = string
 }
-
 variable "postgres_host" {
   description = "The PostgreSQL host from the terraform provider"
   type        = string
   default     = "localhost"
 }
-
 variable "postgres_port" {
   description = "The PostgreSQL port"
   type        = number
@@ -61,11 +58,13 @@ variable "postgres_port" {
 variable "backup_docker_postgres_image" {
   description = "The Docker image used to backup the PostgreSQL database"
   type        = string
+  default     = "postgres:17"
 }
 
 variable "backup_docker_aws_image" {
   description = "The Docker image used to backup the PostgreSQL database to AWS S3"
   type        = string
+  default     = "amazon/aws-cli:2.18.9"
 }
 
 variable "backup_install_path" {
@@ -89,7 +88,7 @@ variable "backup_archive_name" {
 variable "backup_s3_bucket" {
   description = "The S3 bucket to store the backup archive"
   type        = string
-  default = "none"
+  default     = "none"
 }
 
 variable "backup_tmp_dir" {
